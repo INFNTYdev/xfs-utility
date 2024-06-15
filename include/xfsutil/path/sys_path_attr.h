72191bd9576eae23ad4cdfd8011c421126a8e9cf
Jamon Bailey <infinity.dev@outlook.com>
2024-06-15 04:06:11 -0400

    Update sys_path_attr.h

--- include/xfsutil/path/sys_path_attr.h
+++ include/xfsutil/path/sys_path_attr.h
@@ -3,9 +3,9 @@
 #define _XFS_PATH_SYSTEM_PATH_ATTR_H_
 
 typedef struct _SYSTEM_PATH_ATTRIBUTES {
-	bool isLegalStr;
-	bool isDirPath;
-	size_t nodeQ;
+	bool isLegalStr{ false };// Legal path string flag
+	bool isDirPath{ false };// Directory path flag
+	size_t nodeQ{ 0 };// Path node quantity
 } SysPathAttr;
 
 #endif // _XFS_PATH_SYSTEM_PATH_ATTR_H_
