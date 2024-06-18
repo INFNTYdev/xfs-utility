5fc1951f3f28ee1b8a2e9ff3e6f2ea9744daf110
Jamon Bailey <infinity.dev@outlook.com>
2024-06-18 02:43:59 -0400

    Added File Buffer Main Header

--- tests/dev.cpp
+++ tests/dev.cpp
@@ -2,18 +2,11 @@
 #include<iostream>
 #include<fstream>
 
-#include"xfsutil/buffer/interface/buffer_page_i.h"
-#include"xfsutil/buffer/interface/page_in_i.h"
-#include"xfsutil/buffer/interface/page_out_i.h"
+#include"xfsutil/buffer/fbuffer.h"
 
 
 // Maybe? (These could go in the DBuff namespace)
 // That namespace needs a better name, the file as well
-
-//using BufferPage = IBufferPage<char>;// ASCII, ANSI, E-ASCII (8-bit encoding)
-//using BufferPage_u8 = IBufferPage<char8_t>;// ASCII, ANSI, E-ASCII, UTF-8 (8-bit encoding)
-//using BufferPage_u16 = IBufferPage<char16_t>;// UTF-16 (16-bit encoding)
-//using BufferPage_u32 = IBufferPage<char32_t>;// UTF-32 (32-bit encoding)
 
 
 int main(size_t argc, char* argv[])
@@ -36,7 +29,7 @@ int main(size_t argc, char* argv[])
 
 
 	//
-	IBufferPageIn<char> test{ pageSize, 0, 0 };
+	BufferPageIn test{ pageSize, 0, 0 };
 
 	return 0;
 }
